4ebad8405e2f639cad3ab5494b073454c081972c
yuanlevai <yuanlevaileo@gmail.com>
2024-08-19 00:23:41 +0700

    init

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//  toogle vlass avtive
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+}
+
+// hilangkan nav
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+}
+
+// hilangkan navbar diluar sidebar
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+})
